0c11757a9ff9bdae1f3d3826a5346a7ea8f58a59
Vamshi-togglenow <vamshi@togglenow.com>
2026-04-16 18:38:48 +0530

    fix env variable connection

--- server.js
+++ server.js
@@ -1,52 +1,12 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const hana = require("@sap/hana-client");
-
-const app = express();
-app.use(bodyParser.json());
-app.use(express.static("public"));
-
-const port = process.env.PORT || 3000;
-
-// HANA connection (Cloud Foundry will inject via VCAP_SERVICES)
 function getConnection() {
     const conn = hana.createConnection();
-    const hanaEnv = JSON.parse(process.env.VCAP_SERVICES);
-    const service = hanaEnv["hana"][0].credentials;
 
     conn.connect({
-        serverNode: service.host + ":" + service.port,
-        uid: service.user,
-        pwd: service.password,
+        serverNode: process.env.HANA_HOST + ":443",
+        uid: process.env.HANA_USER,
+        pwd: process.env.HANA_PASSWORD,
         encrypt: true
     });
 
     return conn;
 }
-
-// API to insert user
-app.post("/addUser", (req, res) => {
-    const { userId, userName, location } = req.body;
-
-    const conn = getConnection();
-
-    const query = `
-        INSERT INTO "secopsdev"."SUPERUSER"
-        ("USER_ID", "USER_NAME", "LOCATION")
-        VALUES (?, ?, ?)
-    `;
-
-    conn.exec(query, [userId, userName, location], (err) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).send("Insert failed");
-        }
-
-        res.send("User added successfully");
-        conn.disconnect();
-    });
-});
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
